f44db2b548c8a3e98e98bac79658c327d516b3e2
hhh <www.@1603830072.pp.com>
2018-11-22 08:10:49 +0800

    zt-first

--- src/components/list.vue
+++ src/components/list.vue
@@ -9,8 +9,16 @@ export default {
   name: 'list',
   data () {
     return {
-     
+      datalist: [],
+      index: 1
     }
+  },
+  mounted() {
+  	// console.log(this.$route.params.str);
+
+  	fetch(`/BtCApi/List/GetProListWhere?ParentID=4&brand=1450&strWhere=0,0,0,0,0&sort=0&PageIndex=${this.index}&PageSize=20&userID=298969`).then(res=>res.json()).then(res=>{
+	  this.datalist = res.data.Prolist;
+  	})
   }
 }
 </script>
