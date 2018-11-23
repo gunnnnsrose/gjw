dcaaebaf7aff61c947998d3ef2c89e9094a37b13
zoukeduo <1499401370@qq.com>
2018-11-23 10:37:21 +0800

    zkd 两页

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    banner
+    <div class="head">
+    	<router-link to="/home" tag="i" class="iconfont icon-back">
+    		
+    	</router-link>
+    	
+    	<p>【购酒网】福利风暴1019</p>
+    </div>
+    <img src="../assets/fuli.png" alt="">
   </div>
 </template>
 
@@ -17,5 +24,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  
+  .head{
+  	height: .46rem;
+  	width: 100%;
+  	background: #fff;
+  	position: fixed;
+  	top:0;
+  	left:0;
+  	border-bottom: 1px #ccc solid;
+  	i{
+  		color:rgb(153,153,153);
+  		line-height:.46rem;
+  		font-size: .16rem;
+  		position: absolute;
+  		left:.15rem;
+  		font-weight: bold;
+  	}
+  	p{
+  		font-size: .16rem;
+  		line-height: .46rem;
+  		font-family: Arial;
+  		text-align: center;
+  	}
+
+  }
+  img{
+  	width:100%;
+  }
 </style>
